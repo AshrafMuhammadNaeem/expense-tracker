e2336c4631cb51744109438c629ee7e56bb53593
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-05 15:49:25 +0500

    Update GlobalState.js
    created initial sate object with transaction values

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,2 +1,12 @@
 import React from "react"
 import { createContext, useReducer } from "react"
+
+// Creating the Initial State
+const initialState = {
+    transactions:[
+        {id:1, text:'Books', amount: -700},
+        {id:2, text:'Salary', amount: 500000},
+        {id:1, text:'Investments', amount: -200000},
+        {id:1, text:'car', amount: -100000},
+    ]
+}
