da40384611795fe1374158bbfab7d091d710c8c6
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-05 16:18:28 +0500

    GlobalProvider and Appreducer
    created Global provider to access GolbalContext and AppReucer

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,12 +1,29 @@
 import React from "react"
 import { createContext, useReducer } from "react"
+import AppReducer from "./AppReducer";
 
 // Creating the Initial State
 const initialState = {
-    transactions:[
+    transactions: [
         {id:1, text:'Books', amount: -700},
         {id:2, text:'Salary', amount: 500000},
         {id:1, text:'Investments', amount: -200000},
         {id:1, text:'car', amount: -100000},
     ]
+}
+// Create Global Context
+export const GlobalContext = createContext(initialState);
+
+// Create the provider for accesing Global Context
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (<GlobalContext.Provider vallue={
+        {
+        transactions: state.transactions
+
+        }
+    }>
+        {children}
+    </GlobalContext.Provider>);
 }
