daafab8a2de2f773470dd5c6c400cf1df3895b2c
AshrafMuhammadNaeem <67267567+AshrafMuhammadNaeem@users.noreply.github.com>
2021-11-05 15:18:23 +0500

    Update AddTransaction.js
    added useState hook

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
 export const AddTransaction = () => {
-    const [text, setText] = useState("");
+    const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
     return (
         <>
@@ -9,13 +9,15 @@ export const AddTransaction = () => {
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={text} onChange={(e) => setText=(e.target.value)} placeholder="Enter transaction name..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} 
+          placeholder="Enter transaction name..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount <br />
             (Income(+ve), Expenses(-ve) )</label
           >
-          <input type="number" value={amount} onChange={(e) => setAmount=(e.target.value)}  placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} 
+            placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
